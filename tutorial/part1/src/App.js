aed88825a8f51886770b9b1b99834839a21a729e
chirikinareshkumar@gmail.com <chirikinareshkumar@gmail.com>
2021-11-29 23:24:54 +0100

    Corrected and Simplified the  APP

--- tutorial/part1/src/App.js
+++ tutorial/part1/src/App.js
@@ -25,29 +25,36 @@ const App = () => {
     left :0,right:0
   })
 
-  handleLeftClick = () => {
+/* const handleLeftClick = () => {
     const newClicks = {
       left : clicks.left + 1,
       right : clicks.right
     }
-
     setClicks(newClicks)
+  }*/
+
+  const handleLeftClick = () => {
+    setClicks({...clicks, left: clicks.left + 1})
   }
 
   
-  handleRightClick = () => {
+/* const handleRightClick = () => {
     const newClicks = {
       left : clicks.left,
       right : clicks.right + 1
     }
 
     setClicks(newClicks)
+  }*/
+  const handleRightClick = () => {
+    setClicks({...clicks, right: clicks.right + 1})
   }
+
   return(
     <div>
       {clicks.left}
-      <button onClick="{handleLeftClick}">left</button>
-      <button onClick="{handleRightClick}">right</button>
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
       {clicks.right}
     </div>
   )

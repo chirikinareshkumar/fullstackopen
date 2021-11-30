879be8431e0d65cd687e71526ac5c905de87e662
chirikinareshkumar@gmail.com <chirikinareshkumar@gmail.com>
2021-11-30 11:16:36 +0100

    Part 1.11 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,26 +6,45 @@ const Button = ({onClick,text}) => {
   )
 }
 
+const StatisticLine = ({text,value}) => {
+  if(text === "positive"){
+    return(
+      <tr>
+        <td>{text}</td>
+        <td>{value} %</td>
+      </tr>
+    )  
+  }
+  return(
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+    
+  )
+}
 const Statistics = ({good,neutral,bad}) => {
   let all = good + neutral +  bad
 
   if(all === 0){
     return(
       <div>
-        <h2>statistics</h2>
         <p>No feedback given</p>
       </div>
     )
   }
   return (
     <div>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all} </p>
-      <p>average {(good-bad)/all}</p>
-      <p>positive {(good * 100) /all} %</p>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={(good-bad)/all} />
+          <StatisticLine text="positive" value={(good * 100) /all} />
+        </tbody>
+      </table>
     </div>
   )
 }
@@ -41,6 +60,7 @@ function App() {
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
